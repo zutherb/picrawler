b4cf277630e1c2806afb29d918686ab03a31d902
Bernd Zuther <zutherb@users.noreply.github.com>
2022-01-08 10:33:47 +0100

    add delay and debug output

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append(r'/opt/ezblock')
 from ezblock import Pin
+from ezblock import delay
 
 ledValue = 0
 
@@ -12,8 +13,9 @@ def forever():
   global ledValue, userBotton, led
   if userBotton.value() == 0:
     ledValue = (ledValue+1) % 2
-    print("Button pressed")
+    print("Button pressed", ledValue)
     led.value(ledValue)
+    delay(1000)
 
 
 if __name__ == "__main__":
